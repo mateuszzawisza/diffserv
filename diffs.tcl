5baa7c76c925c1ea3ee200548c3c6f5dfdecb24a
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2010-01-24 22:50:13 +0100

    updated files

--- diffs.tcl
+++ diffs.tcl
@@ -57,12 +57,12 @@ $ns duplex-link  $Ed   $D  10Mb   0.01ms DropTail
 
 
 for {set j 1} {$j<=$NodeNb} { incr j } {
- set S($j) [$ns node]
- set E($j) [$ns node]
- $ns duplex-link  $S($j) $E($j)  6Mb   0.01ms DropTail
- $ns simplex-link $E($j) $Core   6Mb   0.1ms dsRED/edge
- $ns simplex-link $Core  $E($j)  6Mb   0.1ms dsRED/core
- $ns queue-limit $S($j) $E($j) 100
+  set S($j) [$ns node]
+  set E($j) [$ns node]
+  $ns duplex-link  $S($j) $E($j)  6Mb   0.01ms DropTail
+  $ns simplex-link $E($j) $Core   6Mb   0.1ms dsRED/edge
+  $ns simplex-link $Core  $E($j)  6Mb   0.1ms dsRED/core
+  $ns queue-limit $S($j) $E($j) 100
 }
 
 #Config Diffserv
@@ -71,7 +71,7 @@ $qEdC       meanPktSize 40
 $qEdC   set numQueues_   1
 $qEdC    setNumPrec      2
 for {set j 1} {$j<=$NodeNb} { incr j } {
- $qEdC addPolicyEntry [$D id] [$S($j) id] TSW2CM 10 $cir0 0.02
+  $qEdC addPolicyEntry [$D id] [$S($j) id] TSW2CM 10 $cir0 0.02
 }
 $qEdC addPolicerEntry TSW2CM 10 11
 $qEdC addPHBEntry  10 0 0 
@@ -95,30 +95,30 @@ $qCEd configQ 0 1 15 45  0.5 0.01
 
 
 for {set j 1} {$j<=$NodeNb} { incr j } {
- set qEC($j) [[$ns link $E($j) $Core] queue]
- $qEC($j) meanPktSize $pktSize
- $qEC($j) set numQueues_   1
- $qEC($j) setNumPrec      2
- $qEC($j) addPolicyEntry [$S($j) id] [$D id] TSW2CM 10 $cir1 0.02
- $qEC($j) addPolicerEntry TSW2CM 10 11
- $qEC($j) addPHBEntry  10 0 0 
- $qEC($j) addPHBEntry  11 0 1 
-# $qEC($j) configQ 0 0 20 40 0.02
- $qEC($j) configQ 0 0 10 20 0.1
- $qEC($j) configQ 0 1 10 20 0.1
+  set qEC($j) [[$ns link $E($j) $Core] queue]
+  $qEC($j) meanPktSize $pktSize
+  $qEC($j) set numQueues_   1
+  $qEC($j) setNumPrec      2
+  $qEC($j) addPolicyEntry [$S($j) id] [$D id] TSW2CM 10 $cir1 0.02
+  $qEC($j) addPolicerEntry TSW2CM 10 11
+  $qEC($j) addPHBEntry  10 0 0 
+  $qEC($j) addPHBEntry  11 0 1 
+ # $qEC($j) configQ 0 0 20 40 0.02
+  $qEC($j) configQ 0 0 10 20 0.1
+  $qEC($j) configQ 0 1 10 20 0.1
 
-$qEC($j) printPolicyTable
-$qEC($j) printPolicerTable
+  $qEC($j) printPolicyTable
+  $qEC($j) printPolicerTable
  
- set qCE($j) [[$ns link $Core $E($j)] queue]
- $qCE($j) meanPktSize      40
- $qCE($j) set numQueues_   1
- $qCE($j) setNumPrec      2
- $qCE($j) addPHBEntry  10 0 0 
- $qCE($j) addPHBEntry  11 0 1 
-# $qCE($j) configQ 0 0 20 40 0.02
- $qCE($j) configQ 0 0 10 20 0.1
- $qCE($j) configQ 0 1 10 20 0.1
+  set qCE($j) [[$ns link $Core $E($j)] queue]
+  $qCE($j) meanPktSize      40
+  $qCE($j) set numQueues_   1
+  $qCE($j) setNumPrec      2
+  $qCE($j) addPHBEntry  10 0 0 
+  $qCE($j) addPHBEntry  11 0 1 
+ # $qCE($j) configQ 0 0 20 40 0.02
+  $qCE($j) configQ 0 0 10 20 0.1
+  $qCE($j) configQ 0 1 10 20 0.1
 
 }
 
@@ -131,17 +131,19 @@ $fmon attach $monfile
 
 #TCP Sources, destinations, connections
 for {set i 1} {$i<=$NodeNb} { incr i } {
-for {set j 1} {$j<=$NumberFlows} { incr j } {
-set tcpsrc($i,$j) [new Agent/TCP/Newreno]
-set tcp_snk($i,$j) [new Agent/TCPSink]
-set k [expr $i*1000 +$j];
-$tcpsrc($i,$j) set fid_ $k
-$tcpsrc($i,$j) set window_ 2000
-$ns attach-agent $S($i) $tcpsrc($i,$j)
-$ns attach-agent $D $tcp_snk($i,$j)
-$ns connect $tcpsrc($i,$j) $tcp_snk($i,$j)
-set ftp($i,$j) [$tcpsrc($i,$j) attach-source FTP]
-} }
+  for {set j 1} {$j<=$NumberFlows} { incr j } {
+    set tcpsrc($i,$j) [new Agent/TCP/Newreno]
+    set tcp_snk($i,$j) [new Agent/TCPSink]
+    set k [expr $i*1000 +$j];
+    $tcpsrc($i,$j) set fid_ $k
+    $tcpsrc($i,$j) set window_ 2000
+    $ns attach-agent $S($i) $tcpsrc($i,$j)
+    $ns attach-agent $D $tcp_snk($i,$j)
+    $ns connect $tcpsrc($i,$j) $tcp_snk($i,$j)
+    set ftp($i,$j) [$tcpsrc($i,$j) attach-source FTP]
+  }
+}
+
 # Generators for random size of files. 
 set rng1 [new RNG]
 $rng1 seed 22
@@ -164,29 +166,30 @@ set t [$RVSize value]
 # Arrivals of sessions follow a Poisson process.
 #
 for {set i 1} {$i<=$NodeNb} { incr i } {
-     set t [$ns now]
+  set t [$ns now]
 
-     for {set j 1} {$j<=$NumberFlows} { incr j } {
-	 # set the beginning time of next transfer from source and attributes
-	 $tcpsrc($i,$j) set sess $j
-	 $tcpsrc($i,$j) set node $i
-	 set t [expr $t + [$RV value]]
-	 $tcpsrc($i,$j) set starts $t
-         $tcpsrc($i,$j) set size [expr [$RVSize value]]
-  $ns at [$tcpsrc($i,$j) set starts] "$ftp($i,$j) send [$tcpsrc($i,$j) set size]"
-  $ns at [$tcpsrc($i,$j) set starts ] "countFlows $i 1"
-
-}}
+  for {set j 1} {$j<=$NumberFlows} { incr j } {
+	  # set the beginning time of next transfer from source and attributes
+	  $tcpsrc($i,$j) set sess $j
+	  $tcpsrc($i,$j) set node $i
+	  set t [expr $t + [$RV value]]
+	  $tcpsrc($i,$j) set starts $t
+    $tcpsrc($i,$j) set size [expr [$RVSize value]]
+    $ns at [$tcpsrc($i,$j) set starts] "$ftp($i,$j) send [$tcpsrc($i,$j) set size]"
+    $ns at [$tcpsrc($i,$j) set starts ] "countFlows $i 1"
+  }
+}
 
 for {set j 1} {$j<=$NodeNb} { incr j } {
-set Cnts($j) 0
+  set Cnts($j) 0
 }   
 
 # The following procedure is called whenever a connection ends
 Agent/TCP instproc done {} {
-global tcpsrc NodeNb NumberFlows ns RV ftp Out tcp_snk RVSize 
-# print in $Out: node, session, start time,  end time, duration,      
-# trans-pkts, transm-bytes, retrans-bytes, throughput   
+  global tcpsrc NodeNb NumberFlows ns RV ftp Out tcp_snk RVSize 
+  # print in $Out: node, session, start time,  end time, duration,      
+  # trans-pkts, transm-bytes, retrans-bytes, throughput   
+
   set duration [expr [$ns now] - [$self set starts] ] 
   set i [$self set node] 
   set j [$self set sess] 
@@ -210,29 +213,34 @@ global tcpsrc NodeNb NumberFlows ns RV ftp Out tcp_snk RVSize
 # it begins, by assigning it 1 (i is passed through the "ind" variable).
 #
 proc countFlows { ind sign } {
-global Cnts Conn NodeNb
-set ns [Simulator instance]
-      if { $sign==0 } { set Cnts($ind) [expr $Cnts($ind) - 1] 
-} elseif { $sign==1 } { set Cnts($ind) [expr $Cnts($ind) + 1] 
-} else { 
-  puts -nonewline $Conn "[$ns now] \t"
-  set sum 0
-  for {set j 1} {$j<=$NodeNb} { incr j } {
-    puts -nonewline $Conn "$Cnts($j) \t"
-    set sum [expr $sum + $Cnts($j)]
+  global Cnts Conn NodeNb
+  set ns [Simulator instance]
+  if { $sign==0 } {
+    set Cnts($ind) [expr $Cnts($ind) - 1] 
+  } 
+  elseif { $sign==1 } {
+    set Cnts($ind) [expr $Cnts($ind) + 1] 
   }
-  puts $Conn "$sum"
-  puts $Conn ""
-  $ns at [expr [$ns now] + 0.2] "countFlows 1 3"
-puts "in count"
-} }
+  else { 
+    puts -nonewline $Conn "[$ns now] \t"
+    set sum 0
+    for {set j 1} {$j<=$NodeNb} { incr j } {
+      puts -nonewline $Conn "$Cnts($j) \t"
+      set sum [expr $sum + $Cnts($j)]
+    }
+    puts $Conn "$sum"
+    puts $Conn ""
+    $ns at [expr [$ns now] + 0.2] "countFlows 1 3"
+    puts "in count"
+  }
+}
 
 #Define a 'finish' procedure
 proc finish {} {
-        global ns tf file2
-        $ns flush-trace
-        close $file2 
-        exit 0
+  global ns tf file2
+  $ns flush-trace
+  close $file2 
+  exit 0
 }         
 
 $ns at 0.5 "countFlows 1 3"
--- router.tcl
+++ router.tcl
@@ -1,37 +1,197 @@
 set ns [new Simulator] 
 
-proc createRouter {ns throughput nodes} {
+set Out [open Out.ns w];   # file containing transfer 
+                           # times of different connections
+set Conn [open Conn.tr w]; # file containing the number of connections 
+
+set tf   [open out.tr w];  # Open the Trace file
+
+set pktSize    1000
+set NodeNb       10; # Number of source nodes
+set throughput 6Mb
+$ns trace-all $tf    
+
+
+#proc setNodes {ns throughput} {
   set Agw [$ns node]
   set Bgw [$ns node]
-  # $ns duplex-link  $S($j) $E($j)  6Mb   0.01ms DropTail
-  $ns simplex-link $Agw $Bgw $throughput 0.1ms dsRED/edge
-  $ns simplex-link $Bgw $Agw $throughput 0.1ms dsRED/core
-  # $ns queue-limit $S($j) $E($j) 100
+  set Core [$ns node]
+set flink [$ns simplex-link $Core $Agw 10Mb 1ms dsRED/core]
+set flink [$ns simplex-link $Core $Bgw 10Mb 1ms dsRED/core]
 
+  $ns simplex-link $Agw $Core $throughput 0.1ms dsRED/edge
+  $ns simplex-link $Core $Agw $throughput 0.1ms dsRED/core
+  $ns queue-limit  $Core $Agw  100
 
-  set queue_AB [[$ns link $Agw $Bgw] queue]
-  $queue_AB       meanPktSize 40
-  $queue_AB   set numQueues_   1
-  $queue_AB    setNumPrec      2
+  $ns simplex-link $Bgw $Core $throughput 0.1ms dsRED/edge
+  $ns simplex-link $Core $Bgw $throughput 0.1ms dsRED/core
+  $ns queue-limit  $Core $Bgw  100
 
-  foreach node $nodes {
-   $queue_AB addPolicyEntry [$Agw id] [$Bgw id] TSW2CM 10 3000 0.02
+  for {set i 1} {$i < 10} {incr i} {
+    set A($i) [$ns node]
+    $ns duplex-link $Agw $A($i) $throughput 0.01ms DropTail
+    $ns queue-limit $A($i) $Agw 100
+
+    set B($i) [$ns node]
+    $ns duplex-link $Bgw $B($i) $throughput 0.01ms DropTail
+    $ns queue-limit $B($i) $Bgw 100
   }
 
-  $queue_AB addPolicerEntry TSW2CM 10 11
-  $queue_AB addPHBEntry  10 0 0 
-  $queue_AB addPHBEntry  11 0 1 
-  $queue_AB configQ 0 0 10 30 0.1
-  $queue_AB configQ 0 1 10 30 0.1
+  #diff serv  
+proc setQueue {ns s d} {
+  set queueSD [[$ns link $s $d] queue]
+  $queueSD       meanPktSize 40
+  $queueSD   set numQueues_   1
+  $queueSD    setNumPrec      2
+
+  $queueSD addPolicyEntry [$s id] [$d id] TSW2CM 10 3000 0.02
+  $queueSD addPolicerEntry TSW2CM 10 11
+  $queueSD addPHBEntry  10 0 0 
+  $queueSD addPHBEntry  11 0 1 
+  $queueSD configQ 0 0 10 30 0.1
+  $queueSD configQ 0 1 10 30 0.1
   
-  $queue_AB printPolicyTable
-  $queue_AB printPolicerTable
+  $queueSD printPolicyTable
+  $queueSD printPolicerTable
+
+  set queueDS [[$ns link $d $s] queue]
+  $queueDS meanPktSize      40
+  $queueDS set numQueues_   1
+  $queueDS setNumPrec      2
+  $queueDS addPHBEntry  10 0 0 
+  $queueDS addPHBEntry  11 0 1 
+  $queueDS configQ 0 0 10 20 0.1
+  $queueDS configQ 0 1 10 20 0.1
+
+
+  return [list $queueSD $queueDS]
+}
+
+  set q1 [setQueue $ns $Bgw $Core]
+  set qBC [lindex $q1 0]
+  set qCB [lindex $q1 1]
+
+  set q2 [setQueue $ns $Agw $Core]
+  set qAC [lindex $q2 0]
+  set qCA [lindex $q2 1]
+
+
+#}
+
+
+  set monfile [open mon.tr w]
+  set fmon [$ns makeflowmon Fid]
+  $ns attach-fmon $flink $fmon
+  $fmon attach $monfile
+  
+  #TCP Sources, destinations, connections
+  for {set i 1} {$i<=$NodeNb} { incr i } {
+    for {set j 1} {$j<=$NumberFlows} { incr j } {
+      set tcpsrc($i,$j) [new Agent/TCP/Newreno]
+      set tcp_snk($i,$j) [new Agent/TCPSink]
+      set k [expr $i*1000 +$j];
+      $tcpsrc($i,$j) set fid_ $k
+      $tcpsrc($i,$j) set window_ 2000
+      $ns attach-agent $S($i) $tcpsrc($i,$j)
+      $ns attach-agent $D $tcp_snk($i,$j)
+      $ns connect $tcpsrc($i,$j) $tcp_snk($i,$j)
+      set ftp($i,$j) [$tcpsrc($i,$j) attach-source FTP]
+    }
+  }
+  
+  # Generators for random size of files. 
+  set rng1 [new RNG]
+  $rng1 seed 22
+  
+  # Random inter-arrival times of TCP transfer at each source i
+  set RV [new RandomVariable/Exponential]
+  $RV set avg_ 0.2
+  $RV use-rng $rng1 
+  
+  # Random size of files to transmit 
+  set RVSize [new RandomVariable/Pareto]
+  $RVSize set avg_ 10000 
+  $RVSize set shape_ 1.25
+  $RVSize use-rng $rng1
+  
+  # dummy command
+  set t [$RVSize value]
+  
+  # We now define the beginning times of transfers and the transfer sizes
+  # Arrivals of sessions follow a Poisson process.
+  #
+  for {set i 1} {$i<=$NodeNb} { incr i } {
+    set t [$ns now]
+  
+    for {set j 1} {$j<=$NumberFlows} { incr j } {
+  	  # set the beginning time of next transfer from source and attributes
+  	  $tcpsrc($i,$j) set sess $j
+  	  $tcpsrc($i,$j) set node $i
+  	  set t [expr $t + [$RV value]]
+  	  $tcpsrc($i,$j) set starts $t
+      $tcpsrc($i,$j) set size [expr [$RVSize value]]
+      $ns at [$tcpsrc($i,$j) set starts] "$ftp($i,$j) send [$tcpsrc($i,$j) set size]"
+      $ns at [$tcpsrc($i,$j) set starts ] "countFlows $i 1"
+    }
+  }
+  
+  for {set j 1} {$j<=$NodeNb} { incr j } {
+    set Cnts($j) 0
+  }   
+  
+  # The following procedure is called whenever a connection ends
+  Agent/TCP instproc done {} {
+    global tcpsrc NodeNb NumberFlows ns RV ftp Out tcp_snk RVSize 
+    # print in $Out: node, session, start time,  end time, duration,      
+    # trans-pkts, transm-bytes, retrans-bytes, throughput   
+  
+    set duration [expr [$ns now] - [$self set starts] ] 
+    set i [$self set node] 
+    set j [$self set sess] 
+    set time [$ns now] 
+    puts $Out "$i \t $j \t $time \t\
+        $time \t $duration \t [$self set ndatapack_] \t\
+        [$self set ndatabytes_] \t [$self set  nrexmitbytes_] \t\
+        [expr [$self set ndatabytes_]/$duration ]"    
+  
+  	  # update the number of flows
+        countFlows [$self set node] 0
+  
+  }
 }
 
 
-set a [$ns node]
-set b [$ns node]
-set c [$ns node]
-set nodes [list a b c]
+proc countflows { ind sign } {
+  global Cnts Conn NodeNb
+  set ns [Simulator instance]
+  if { $sign==0 } {
+    set Cnts($ind) [expr $Cnts($ind) - 1] 
+  } 
+  elseif { $sign==1 } {
+    set Cnts($ind) [expr $Cnts($ind) + 1] 
+  }
+  else { 
+    puts -nonewline $Conn "[$ns now] \t"
+    set sum 0
+    for {set j 1} {$j<=$NodeNb} { incr j } {
+      puts -nonewline $Conn "$Cnts($j) \t"
+      set sum [expr $sum + $Cnts($j)]
+    }
+    puts $Conn "$sum"
+    puts $Conn ""
+    $ns at [expr [$ns now] + 0.2] "countFlows 1 3"
+    puts "in count"
+  }
+}
 
-createRouter $ns 6Mb $nodes
+#Define a 'finish' procedure
+proc finish {} {
+  global ns tf file2
+  $ns flush-trace
+  close $file2 
+  exit 0
+}         
+
+#setNodes $ns 6Mb
+
+  $ns run
