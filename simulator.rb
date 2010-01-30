56028d1c77394affbc28ed63f6a00d3bcc05a450
Jacek Szarski <jacek.szarski@gmail.com>
2010-01-30 19:18:33 +0100

    changer router.rb methods into a class

--- simulator.rb
+++ simulator.rb
@@ -7,15 +7,35 @@ VARIABLES_MAP = {
   :link_throughput => 'THROUGHPUT'
 }
 
-def run_simulation(options={})
-  variables = options.collect do |variable_name, value|
-    env_variable_name = VARIABLES_MAP[variable_name]
-    raise Exception.new("Variable #{variable_name} is not defined!") unless env_variable_name
-    "#{env_variable_name}='#{value}'"
+class Simulation
+  attr_accessor :command, :output
+
+  def self.run!(options={})
+    sim = self.new(options)
+    sim.run
+    return sim
   end
-  system "#{variables.join(" ")} #{SIMULATION_COMMAND}"
+
+  def initialize(options={})
+    variables = options.collect do |variable_name, value|
+      env_variable_name = VARIABLES_MAP[variable_name]
+      raise Exception.new("Variable #{variable_name} is not defined!") unless env_variable_name
+      "#{env_variable_name}='#{value}'"
+    end
+    self.command = "#{variables.join(" ")} #{SIMULATION_COMMAND}"
+    puts "Created command:\n  #{self.command}"
+  end
+
+  def run
+    puts "Issuing:\n  #{self.command}"
+    self.output = eval "%x[#{ self.command }]"
+  end
+  
+  def parse_outuput
+    puts self.output
+  end
 end
 
 puts "Running simulations..."
-run_simulation :node_count => 3#, :packet_size => 100, :flows_count => 100, :link_throughput => '6Mb'
+simulation = Simulation.run! :node_count => 3, :packet_size => 100, :flows_count => 10#, :link_throughput => '6Mb'
 puts "da enda"
