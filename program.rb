3358aaccac66b751bda523bff4228bb504666a20
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2010-02-01 22:32:15 +0100

    working simulation with edge and core routers

--- program.rb
+++ program.rb
@@ -4,11 +4,11 @@ q1 = [[10, 20, 0.1,10, 20, 0.1], [10, 20, 0.1, 10, 20, 0.1], [10, 20, 0.1, 10, 2
 q2 = [[10, 20, 0.9,10, 20, 0.9], [10, 20, 0.9, 10, 20, 0.9], [10, 20, 0.9, 10, 20, 0.9]]
 
 settings = { :node_count => 3,
-             :packet_size => 10000,
+             :packet_size => 1000,
              :flows_count => 1000,
-             :link_throughput => '5Mb',
+             :link_throughput => '0.5Mb',
              :cir => 100, :pir => 100,
-             :average_source_delay => '0.003'
+             :average_source_delay => '0.01'
            }
 
 simulation1 = Simulation.new settings.merge({:queue_settings => q1})
