c56c18872cca1bb17584dc64a720cb9c2340a1b8
Jacek Szarski <jacek.szarski@gmail.com>
2010-02-01 13:53:03 +0100

    split some settings to be more readable

--- program.rb
+++ program.rb
@@ -2,15 +2,20 @@ require 'simulation'
 q1 = [[10, 20, 0.1,10, 20, 0.1], [10, 20, 0.1, 10, 20, 0.1], [10, 20, 0.1, 10, 20, 0.1]]
 q2 = [[10, 20, 0.9,10, 20, 0.9], [10, 20, 0.9, 10, 20, 0.9], [10, 20, 0.9, 10, 20, 0.9]]
 
-simulation1 = Simulation.new :node_count => 3, :packet_size => 10000, :flows_count => 1000, :queue_settings => q1, :link_throughput => '5Mb', :cir => 100, :pir => 10000, :average_source_delay => '0.01'
-simulation2 = Simulation.new :node_count => 3, :packet_size => 10000, :flows_count => 1000, :queue_settings => q2, :link_throughput => '5Mb', :cir => 100, :pir => 10000, :average_source_delay => '0.01'
+settings = { :node_count => 3,
+             :packet_size => 10000,
+             :flows_count => 1000,
+             :link_throughput => '5Mb',
+             :cir => 100, :pir => 10000,
+             :average_source_delay => '0.003'
+           }
+
+simulation1 = Simulation.new settings.merge({:queue_settings => q1})
+simulation2 = Simulation.new settings.merge({:queue_settings => q2})
 
 simulation1.run and simulation2.run
 
-puts "\nFirst simulation:"
-puts simulation1.result.inspect
-puts "\nSecond simulation:"
-puts simulation2.result.inspect
+puts "\nFirst simulation:", simulation1.result.inspect, "\nSecond simulation:", simulation2.result.inspect
 
 #puts simulation1.output
 #puts simulation2.output
